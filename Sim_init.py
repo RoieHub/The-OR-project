6c8a71f40147ceabc2caf22cf928aaae4aaa61f2
roie <roiemalykin@gmail.com>
2021-08-15 14:22:12 +0300

    added constructor to Implemintation Sim_init.py

--- Sim_init.py
+++ Sim_init.py
@@ -1,5 +1,6 @@
 
 # This class hold Several parameters to init a simulation
+import osmnx as ox
 import datetime
 
 class Sim_init:
@@ -9,4 +10,12 @@ class Sim_init:
     #Times
     #datetime.timedelta documentation :A duration expressing the difference between two date, time, or datetime instances to microsecond resolution.
     max_delay : datetime.timedelta # The difference between the drop off time and minimal time to reach destination.==: delta = Request.toed - Requst.toe
+    #map : dafaultivly 'Raanana, Israel'
+
+    # This is default constructor in raanana city
+    def __init__(self):
+        self.fleet_size = 1
+        self.v_cap = 10
+        self.max_delay = datetime.timedelta(minutes=10)
+        self.map = ox.graph_from_place('Raanana, Israel', network_type='drive')
 
