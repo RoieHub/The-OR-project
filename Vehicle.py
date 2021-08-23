2b1b760c16c14fbfff1c3c0ebeb8aebcf4fa3e21
roie <roiemalykin@gmail.com>
2021-08-23 14:00:52 +0300

    Added methods add passenger and remove passenger for Vehicle.py

--- Vehicle.py
+++ Vehicle.py
@@ -1,25 +1,38 @@
 import datetime
 import Request
-class Vehicle:
-    #Vehicle id? - may be added
-    class_counter = 0
-    cp : int # Current possition , location node.
-    ct : datetime.datetime # Current time registered in the vehicle.
-    passengers : list # Dynamic list of requests that been picked up by this vehicle.
 
-    def __init__(self , start_node):
+"""The current state of a vehicle v is given by a tuple {current_position , current_time , passengers}
+    A passenger p is a request that has been picked-up by a vehicle.
+"""
+class Vehicle:
+    class_counter = 0
+
+    def __init__(self , start_node, passengers=None):
         self.id = Vehicle.class_counter
         Vehicle.class_counter += 1
-        self.ct = datetime.datetime.now()
+        self.curr_time = datetime.datetime.now()
+        self.curr_pos = start_node
+        if passengers is None:
+            self.passengers = []
 
     #This method returns the Node ID of the nearest node.
-    def curr_pos(self):
-        return self.cp
+    def get_curr_pos(self):
+        return self.curr_pos()
+
+    def set_curr_pos(self,pos):
+        self.curr_pos = pos
+        return
+
     def add_passenger(self,req):
         self.passengers.append(req)
-
-    def __str__(self):
-      #  print [str(Request.id) for p in self.passengers]
         return
+
+    #This method removes the passenger object given to the func
+    def rm_passenger(self, passenger):
+        if self.passengers is not None :
+            self.passengers.remove(passenger)
+        return
+
+
 
 
