73059be5680d321d106118a9bc951624a38d16f5
roie <roiemalykin@gmail.com>
2021-08-30 17:20:19 +0300

    TripAlgo.py init

---
+++ TripAlgo.py
@@ -0,0 +1,12 @@
+import Request
+# Private case two requests (Mainly for RV graph)
+
+def validTrip2(r1 , r2 , map) :
+    # Check both trips Z constraints
+
+    #Cacl travel times from r1 to r2
+    if()
+    #Cacl travel times from r2 to r1
+
+
+
