321169b93098ceb888afb7ddd98a9fda6fca2851
op1995 <pellerofir@gmail.com>
2021-08-31 00:14:43 +0300

    added to TripAlgo the start of checking if 2 requests can ride together (for RV graph)

--- TripAlgo.py
+++ TripAlgo.py
@@ -1,12 +1,43 @@
 import Request
+import networkx as nx
+
+
 # Private case two requests (Mainly for RV graph)
 
-def validTrip2(r1 , r2 , map) :
+def validTrip2(r1, r2, map):
     # Check both trips Z constraints
 
-    #Cacl travel times from r1 to r2
-    if()
-    #Cacl travel times from r2 to r1
+    # First we make sure that the first and most important condition is true
+    # That is - if a virtual taxi, starting from r1 drives to r2, will it get
+    # there in time? (i.e. - before r2.latest_time_to_pick_up)
+
+    # Cacl travel times and time of arrival driving from r1 to r2
+    # nx.algorithms.shortest_paths.generic.shortest_path_length
+    r1S_to_r2S_pathDuration = nx.shortest_path_length(map, r1.origin, r2.origin, weight='travel_time')
+    time_of_arrival_to_r2 = datetime.datetime.now() + datetime.timedelta(seconds=r1_to_r2_pathDuration)
+
+    #in case we can't reach r2.origin in time
+    if time_of_arrival_to_r2 > r2.latest_time_to_pick_up:
+        return False, -1
+
+    #now we need to check that a permutation exists where -
+    # 1 - r2 doesn't wait until after his latest_time_to_pick_up
+    # 2 - both r1 and r2 reach their respective destinations without
+
+    #first we calculate all the relevant parts of paths to take
+    r1S_to_r1D_pathDuration = nx.shortest_path_length(map, r1.origin, r1.dest, weight='travel_time')
+
+    r2S_to_r2D_pathDuration = nx.shortest_path_length(map, r2.origin, r2.dest, weight='travel_time')
+    r2S_to_r1D_pathDuration = nx.shortest_path_length(map, r2.origin, r1.dest, weight='travel_time')
+
+    r1D_to_r2S_pathDuration = nx.shortest_path_length(map, r1.dest, r2.origin, weight='travel_time')
+    r1D_to_r2D_pathDuration = nx.shortest_path_length(map, r1.dest, r2.origin, weight='travel_time')
+
+    r2D_to_r1D_pathDuration = nx.shortest_path_length(map, r2.dest, r1.dest, weight='travel_time')
 
 
+
+    # r1 r1 r2 r2
+    # r1 r2 r1 r2
+    # r1 r2 r2 r1
 
