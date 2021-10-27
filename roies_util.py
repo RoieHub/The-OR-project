ba02fbb23ffb0e59dfd8ef67b5fd0662885e27a8
roie <roiemalykin@gmail.com>
2021-10-27 17:25:10 +0300

    str_to_time is now work directly with 'DateTime' module instead of "pd.Timestamp"

--- roies_util.py
+++ roies_util.py
@@ -1,6 +1,12 @@
-from pandas import Timestamp
+import datetime
 
-
+# Old ver , with pandas middleman middleman
 def str_to_time(s):
+    t = datetime.strptime(s, '%y-%m-%d %H:%M:%S')
+    return t
+
+
+
+'''def str_to_time(s):
     t = Timestamp(s)
-    return t.to_pydatetime()
+    return t.to_pydatetime()'''
